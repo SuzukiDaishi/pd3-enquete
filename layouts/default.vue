eb1c01423884931fccaee07ad2df6c9faa406b8f
SuzukiDaishi <zukky.rikugame@gmail.com>
2021-10-21 19:19:31 +0900

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,18 @@
       .hero-body
         h1.title PD3 アンケート調査
         p.subtitle 金沢工業大学 4EP3 鈴木大志
-    Nuxt
+    //- Nuxt
+    Junbi
     footer.footer
   </div>
 </template>
+
+<script>
+import Junbi from '@/components/Junbi.vue'
+
+export default {
+  components: {
+    Junbi,
+  }
+}
+</script>
